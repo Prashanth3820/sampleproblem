5b36b402ad07767a17a3630b0deb9636db7efba7
Prashanth3820 <prashanth83362@gmail.com>
2024-09-12 20:41:26 +0530

    Sample project is uploaded

---
+++ src/test/java/org/g/p/GitLink.java
@@ -0,0 +1,13 @@
+package org.g.p;
+
+public class GitLink {
+	private void method() {
+		System.out.println("Method 1...............");
+	}
+	
+	private void methods() {
+		System.out.println("Method 2................");
+	}
+	
+
+}
